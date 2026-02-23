b6e0e60945d4111f256d1e58590bdcbfb5204a2d
Amigo20057 <denrizuk@gmail.com>
2026-02-23 19:03:47 +0200

    update files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,18 +2,19 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import ordersRouter from './routes/orders.routes';
-import errorHandler from './middlewares/errorHandler';
+import { OrderController } from './orders/order.controller';
+import 'dotenv/config';
 
 const app = express();
+const port = process.env.PORT;
 
 app.use(helmet());
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/orders', ordersRouter);
+app.use('/api/orders', OrderController);
 
-app.use(errorHandler);
-
-export default app;
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
